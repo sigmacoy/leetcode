2af5038f2633df6de677e5cba87de7fe77e25acc
macooyb <macoybacalla9@gmail.com>
2025-12-21 02:50:17 +0800

    prep files

---
+++ 2_TwoPointer/Fast_Slow_(CycleDetection)/find-the-duplicate-number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        slow = nums[0];
+        while (slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow; // pwede pud return fast
+    }
+};
+// time O(n)
+// space O(1)
---
+++ 2_TwoPointer/Fast_Slow_(CycleDetection)/is-subsequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.empty()) return true;
+        int i = 0;
+        int count = 0;
+        
+        while(i < t.length() && count < s.length()){
+            if(s[count] == t[i]){
+                count++;
+            }
+            i++;
+        }
+
+        return count == s.length();
+    }
+};
+// time O(n)
+// space O(1)
