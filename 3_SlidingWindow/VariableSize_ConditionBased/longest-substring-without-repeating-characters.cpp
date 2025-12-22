d971d1624bbd8600c43d2f7029ed4ec032ea6aa7
macooyb <macoybacalla9@gmail.com>
2025-12-23 05:21:59 +0800

    done for today

--- 3_SlidingWindow/VariableSize_ConditionBased/longest-substring-without-repeating-characters.cpp
+++ 3_SlidingWindow/VariableSize_ConditionBased/longest-substring-without-repeating-characters.cpp
@@ -1,1 +1,23 @@
 #include <bits/stdc++.h>
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        unordered_set<char> uSet;
+        int l = 0, r = 0;
+        int maxLen = 0;
+        while(l < n && r < n){
+            if(uSet.count(s[r])){
+                uSet.erase(s[l]);
+                l++;
+            } else {
+                uSet.insert(s[r]);
+                r++;
+                maxLen = max(maxLen, r - l);
+            }
+        }
+        return maxLen;
+    }
+};
+// time O(n)
+// space O(n)
