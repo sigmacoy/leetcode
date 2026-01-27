f6881ab9e97694a35a07ca5cc80236ef3f6cfe5b
macooyb <macoybacalla9@gmail.com>
2026-01-27 22:53:07 +0800

    yes

--- 5_Stack/MonotonicStack/daily-temperatures.cpp
+++ 5_Stack/MonotonicStack/daily-temperatures.cpp
@@ -2,17 +2,18 @@
 class Solution {  
 public:  
     vector<int> dailyTemperatures(vector<int>& temperatures) {  
-        int n = temperatures.size();  
-        vector<int> res(n, 0);  
-        stack<int> st;  
-        for (int i = 0; i < n; i++) {  
-            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {  
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        stack<int> st;
+
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]){
                 res[st.top()] = i - st.top();
-                st.pop();  
+                st.pop();
             }
-            st.push(i);  
+            st.push(i);
         }
 
-        return res;  
+        return res;
     }
 };
