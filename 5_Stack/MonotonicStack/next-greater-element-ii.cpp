21576f94c4d4967640d3e67c10eab4513bb86f54
macooyb <macoybacalla9@gmail.com>
2026-01-27 21:56:53 +0800

    done leetcode today

--- 5_Stack/MonotonicStack/next-greater-element-ii.cpp
+++ 5_Stack/MonotonicStack/next-greater-element-ii.cpp
@@ -1,1 +1,21 @@
 #include <bits/stdc++.h>
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr(n, -1);
+        stack<int> st;
+
+        for(int i = 2 * n - 1; i >= 0; i--){
+            while(!st.empty() && nums[i % n] >= st.top())
+                st.pop();
+            
+            if(!st.empty() && i < n)
+                arr[i] = st.top();
+
+            st.push( nums[i % n] );
+        }
+
+        return arr;
+    }
+};
