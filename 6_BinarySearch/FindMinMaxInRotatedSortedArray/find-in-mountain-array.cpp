a0ec74e7ca5a6f0b0de9a5d11945fd7a5c440f9f
macooyb <macoybacalla9@gmail.com>
2026-04-06 19:23:23 +0800

    continued binary search practice problems

--- 6_BinarySearch/FindMinMaxInRotatedSortedArray/find-in-mountain-array.cpp
+++ 6_BinarySearch/FindMinMaxInRotatedSortedArray/find-in-mountain-array.cpp
@@ -1,2 +1,64 @@
 #include <bits/stdc++.h>
 
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+ class MountainArray {
+    public:
+     int get(int index);
+     int length();
+ };
+
+class Solution {
+    int peakIndex(MountainArray &mountainArr){
+        int n = mountainArr.length();
+        int L = 0, R = n - 1;
+        while(L < R){
+            int mid = L + (R - L) / 2;
+            if(mountainArr.get(mid) < mountainArr.get(mid + 1)){
+                L = mid + 1;
+            } else R = mid;
+        }
+        return L;
+    }
+    int BSLeft(MountainArray &mountainArr, int L, int R, int target){
+        int mid;
+        while(L <= R){
+            mid = L + (R - L) / 2;
+            if(mountainArr.get(mid) == target) return mid;
+            else if(mountainArr.get(mid) < target){
+                L = mid + 1;
+            } else R = mid - 1;
+        }
+        return -1;
+    }
+    int BSRight(MountainArray &mountainArr, int L, int R, int target){
+        int mid;
+        while(L <= R){
+            mid = L + (R - L) / 2;
+            if(mountainArr.get(mid) == target) return mid;
+            else if(mountainArr.get(mid) > target){
+                L = mid + 1;
+            } else R = mid - 1;
+        }
+        return -1;
+    }
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        int pIndex = peakIndex(mountainArr);
+        int leftRes = BSLeft(mountainArr, 0, pIndex, target);
+        if(leftRes != -1){
+            return leftRes;
+        }
+        int rightRes = BSRight(mountainArr, pIndex, n - 1, target);
+        return rightRes;
+    }
+};
--- 6_BinarySearch/FindMinMaxInRotatedSortedArray/find-peak-element.cpp
+++ 6_BinarySearch/FindMinMaxInRotatedSortedArray/find-peak-element.cpp
@@ -1,2 +1,16 @@
 #include <bits/stdc++.h>
 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int L = 0, R = nums.size() - 1;
+        while(L < R){
+            int mid = L + (R - L) / 2;
+            if(nums[mid] > nums[mid + 1]){
+                R = mid;
+            } else L = mid + 1; // way labot mid value
+        }
+
+        return L; 
+    }
+};
