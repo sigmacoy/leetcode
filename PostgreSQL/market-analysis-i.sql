ae00914577488205133f3a60dad305ad533f9356
macooyb <macoybacalla9@gmail.com>
2026-04-07 13:57:42 +0800

    one pgsql problem

---
+++ PostgreSQL/market-analysis-i.sql
@@ -0,0 +1,62 @@
+-- Write your PostgreSQL query statement below
+
+/*
+buyer_id join_date orders_in_2019
+naa 0 count sa output gina include
+
+left join
+filter year
+count orders per user
+select required cols
+group by user
+*/
+
+SELECT
+    u.user_id AS buyer_id,
+    u.join_date,
+    COUNT(o.order_id) AS orders_in_2019
+FROM
+    Users u
+LEFT JOIN Orders o
+    ON u.user_id = o.buyer_id
+    AND EXTRACT(YEAR FROM o.order_date) = 2019
+
+GROUP BY
+    u.user_id, u.join_date
+ORDER BY
+    u.user_id
+
+
+
+
+Input: 
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2018-01-01 | Lenovo         |
+| 2       | 2018-02-09 | Samsung        |
+| 3       | 2018-01-19 | LG             |
+| 4       | 2018-05-21 | HP             |
++---------+------------+----------------+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2018-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2018-08-04 | 1       | 4        | 2         |
+| 5        | 2018-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+
+Output: 
++-----------+------------+----------------+
+| buyer_id  | join_date  | orders_in_2019 |
++-----------+------------+----------------+
+| 1         | 2018-01-01 | 1              |
+| 2         | 2018-02-09 | 2              |
+| 3         | 2018-01-19 | 0              |
+| 4         | 2018-05-21 | 0              |
++-----------+------------+----------------+
--- PostgreSQL/string-related/1_concat.md
+++ PostgreSQL/string-related/1_concat.md
@@ -8,3 +8,11 @@ LEFT(..., 1) → extracts first character
 UPPER(...) → capitalizes that first character
 
 SUBSTRING(LOWER(name) FROM 2) → extracts from 2nd char onward (lowercase)
+
+EXTRACT(YEAR FROM o.order_date) = 2019 
+= returns numeric
+= SQL standard
+
+DATE_PART('year', o.order_date) = 2019
+= returns float8 (double precision)
+= Not specific to PostgreSQL
